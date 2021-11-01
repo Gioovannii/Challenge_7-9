d7ae8c7f861391e7bde1f241b71014b8840ac86e
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-01 13:41:22 +0800

    Set a nice background color

--- Challenge_7-9/ContentView.swift
+++ Challenge_7-9/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Color(red: 0.184, green: 0.212, blue: 0.251)
+                .ignoresSafeArea()
+            VStack {
+                Text("Hello, world !")
+                    .foregroundColor(.white)
+                    .padding()
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
     }
 }
