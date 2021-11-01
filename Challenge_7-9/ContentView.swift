51edcd21f12532eedaaaa3fd5ba152e47d49788d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-01 14:07:55 +0800

    Change background for light and dark mode

--- Challenge_7-9/ContentView.swift
+++ Challenge_7-9/ContentView.swift
@@ -11,13 +11,13 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Color(red: 0.184, green: 0.212, blue: 0.251)
+            Color("Color")
                 .ignoresSafeArea()
             VStack {
                 Text("Hello, world !")
-                    .foregroundColor(.white)
+                    .foregroundColor(Color("ColorText"))
                     .padding()
-                
+            
             }
         }
     }
@@ -25,19 +25,11 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-    }
-}
-
-
-extension Color {
-    init(hex: UInt, alpha: Double = 1) {
-        self.init(
-            .sRGB,
-            red: Double((hex >> 16) & 0xff) / 255,
-            green: Double((hex >> 08) & 0xff) / 255,
-            blue: Double((hex >> 00) & 0xff) / 255,
-            opacity: alpha
-        )
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+            
     }
 }
