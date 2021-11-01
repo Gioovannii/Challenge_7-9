79a70e28ce3ca597c0012cfc726290eb202e901b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-01 11:51:26 +0800

    Initial Commit

---
+++ Challenge_7-9/Challenge_7_9App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_7_9App.swift
+//  Challenge_7-9
+//
+//  Created by Giovanni Gaffé on 2021/11/1.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_7_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge_7-9/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge_7-9
+//
+//  Created by Giovanni Gaffé on 2021/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
