d695a3756c1dfba1263d619af45debc779af6206
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-01 15:07:14 +0800

    add some assets

--- Challenge_7-9/ContentView.swift
+++ Challenge_7-9/ContentView.swift
@@ -11,6 +11,7 @@ struct Dreams: Identifiable, Codable {
     var id = UUID()
     let name: String
     let type: String
+    let image: String
     let description: String
 }
 
@@ -22,15 +23,35 @@ struct ContentView: View {
     @ObservedObject var activities = Activities()
     @State private var showDreams = false
     var body: some View {
-        
         ZStack {
             Color("Color")
                 .ignoresSafeArea()
-            VStack {
-                Text("Hello, world !")
-                    .foregroundColor(Color("ColorText"))
-                    .padding()
-            
+            NavigationView {
+                List {
+                    ForEach(activities.items) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+
+                            }
+                            
+                            Spacer()
+                            Image(item.image)
+                        }
+                    }
+                }
+                .navigationBarItems(leading: EditButton(),
+                                    trailing:
+                                        Button(action: {
+                    let exampleActivity = Dreams(id: UUID(), name: "First dream", type: "dream", image: "sad", description: "A short description of a dream")
+                    activities.items.append(exampleActivity)
+                    // more code to come
+                }) {
+                    Image(systemName: "plus")
+                })
+                .navigationTitle("Dream Reminder")
             }
         }
     }
@@ -40,8 +61,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
-            ContentView()
-                .preferredColorScheme(.dark)
         }
             
     }
